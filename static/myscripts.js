64761113d6b9daeaf5b35c3682b007802812ee95
Anuj1942 <114324736+Anuj1942@users.noreply.github.com>
2022-10-03 09:50:36 +0530

    Add files via upload
    Done

---
+++ static/myscripts.js
@@ -0,0 +1,29 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 500){
+//             $('.scroll-up-btn').addClass("show");
+//         }
+//         else{
+//             $('.scroll-up-btn').removeClass("show");
+//         }
+//     })
+// });
+
+// $('.scroll-up-btn').click(function(){
+//     $('html').animate({scrollTop: 0});
+// });
+
+var typed = new Typed(".typing", {
+    strings: ["Developer","Coder"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typ", {
+    strings: ["Devloper","Coder"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
